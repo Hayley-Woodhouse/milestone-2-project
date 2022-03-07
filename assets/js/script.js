b093b207240bc3b4c9d6fbdc357a813c375154e1
Hayley-Woodhouse <h.woodhouse639@gmail.com>
2022-03-07 16:22:09 +0000

    added function for hamburger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function diffFunction() {
@@ -7,13 +8,33 @@ function diffFunction() {
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      let i;
       for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+        let openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
       }
     }
   }
+
+  function hamFunction() {
+    document.getElementById("hamDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.hamDropbtn')) {
+      let dropdowns = document.getElementsByClassName("ham-dropdown-content");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
